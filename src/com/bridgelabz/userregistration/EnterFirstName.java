63f5efe4bfea652a3ed086d267dd56ecd3004ea9
hp-pc <navyashree811@gmail.com>
2022-08-26 18:06:20 +0530

    Added : Enter Valid First Name

---
+++ src/com/bridgelabz/userregistration/EnterFirstName.java
@@ -0,0 +1,20 @@
+/*
+ * UC1 : Valid First Name with first letter starting with capital and has minimum 3 characters in it.
+ */
+package com.bridgelabz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class EnterFirstName {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration system using Regex");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your First Name : ");
+		String firstName = sc.nextLine();
+		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}", firstName));
+
+	}
+
+}
