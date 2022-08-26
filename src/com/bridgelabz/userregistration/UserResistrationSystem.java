7a072a03f3c5739f5b284410b14b2159331f1e4f
hp-pc <navyashree811@gmail.com>
2022-08-26 18:14:55 +0530

    Added : Enter Valid Last Name

--- src/com/bridgelabz/userregistration/UserResistrationSystem.java
+++ src/com/bridgelabz/userregistration/UserResistrationSystem.java
@@ -6,7 +6,7 @@ package com.bridgelabz.userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class EnterFirstName {
+public class UserResistrationSystem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration system using Regex");
 		Scanner sc = new Scanner(System.in);
@@ -14,6 +14,10 @@ public class EnterFirstName {
 		System.out.println("Enter your First Name : ");
 		String firstName = sc.nextLine();
 		System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}", firstName));
+
+		System.out.println("Enter your Last Name : ");
+		String lastName = sc.nextLine();
+		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
 
 	}
 
